d3d0d910835d002141595d40b89931365106a063
willrhoda4 <willrhoda4@gmail.com>
2024-03-25 22:28:39 -0500

    fine tuning 6

--- server/server.ts
+++ server/server.ts
@@ -139,7 +139,7 @@ app.use(async (req, res, next) => {
 
 // Catch-all route to serve React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 } );
 
 
